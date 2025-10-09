1403b5e5cb84e076453a9d7ccff6ea2c7296bfab
Charle Eromose <okuekhamhene@gmail.com>
2025-10-09 21:13:41 +0100

    updated backend

--- api/settings.py
+++ api/settings.py
@@ -124,8 +124,8 @@ DATABASES = {
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',  # <-- add this
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
 
--- src/hooks/useLogin.ts
+++
@@ -1,9 +0,0 @@
-import usePostData from "./usePostData";
-
-interface LoginPayload {
-    email: string;
-    password: string;
-}
-
-const useLogin = () => usePostData<LoginPayload>("/auth/jwt/create/");
-export default useLogin;
