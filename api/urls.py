a15f1863aeab72521a2f4c21791102840a12510a
woletokun <85924936+woletokun@users.noreply.github.com>
2025-10-14 17:34:12 +0100

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/',include('userauth.urls')),
-    path('api/apps/',include('applications.urls')),
+    path('api/user/', include('userauth.urls')),
+    path('api/apps/', include('applications.urls')),
 ]
